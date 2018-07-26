ec22e93e94c40f2511130b66c2aca86513e23cfd
jayheiland <>
2018-07-26 05:39:27 -0600

    Gets wifi password from user, then hides keyboard

--- AirU_Setup_Assistant_2/app/src/main/java/com/airu/airu_setup_assistant_2/WiFiDemo.java
+++ AirU_Setup_Assistant_2/app/src/main/java/com/airu/airu_setup_assistant_2/WiFiDemo.java
@@ -10,16 +10,23 @@ import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -31,136 +38,20 @@ public class WiFiDemo extends Activity implements View.OnClickListener
 {
     WifiManager wifi;
     ListView lv;
-    TextView textStatus;
+    TextView wifiListHeader;
     TextView airuDisplay;
     Button buttonScan;
     int size = 0;
     ScanResult airu_result;
-    List<ScanResult> network_results = new List<ScanResult>() {
-        @Override
-        public int size() {
-            return 0;
-        }
+    List<ScanResult> network_results;
+    String selected_wifi_ssid;
+    String selected_wifi_password;
+    EditText passwordField;
+    Button enterPassword;
 
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @NonNull
-        @Override
-        public Iterator<ScanResult> iterator() {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @NonNull
-        @Override
-        public <T> T[] toArray(@NonNull T[] a) {
-            return null;
-        }
-
-        @Override
-        public boolean add(ScanResult scanResult) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(@NonNull Collection<? extends ScanResult> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(int index, @NonNull Collection<? extends ScanResult> c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public ScanResult get(int index) {
-            return null;
-        }
-
-        @Override
-        public ScanResult set(int index, ScanResult element) {
-            return null;
-        }
-
-        @Override
-        public void add(int index, ScanResult element) {
-
-        }
-
-        @Override
-        public ScanResult remove(int index) {
-            return null;
-        }
-
-        @Override
-        public int indexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public int lastIndexOf(Object o) {
-            return 0;
-        }
-
-        @NonNull
-        @Override
-        public ListIterator<ScanResult> listIterator() {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public ListIterator<ScanResult> listIterator(int index) {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public List<ScanResult> subList(int fromIndex, int toIndex) {
-            return null;
-        }
-    };
 
     String ITEM_KEY = "key";
-    ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> wifinameslist = new ArrayList<HashMap<String, String>>();
     SimpleAdapter adapter;
 
     /* Called when the activity is first created. */
@@ -169,12 +60,37 @@ public class WiFiDemo extends Activity implements View.OnClickListener
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setTitle("AirU Sensor Setup");
 
-        textStatus = (TextView) findViewById(R.id.textStatus);
-        airuDisplay = (TextView) findViewById(R.id.airuDisplay);
-        buttonScan = (Button) findViewById(R.id.buttonScan);
+        wifiListHeader = findViewById(R.id.wifiListHeader);
+        airuDisplay = findViewById(R.id.airuDisplay);
+        buttonScan = findViewById(R.id.buttonScan);
+        passwordField = findViewById(R.id.passwordField);
+        enterPassword = findViewById(R.id.enterPassword);
+
+        //hide the password field and button at first
+        passwordField.setVisibility(TextView.INVISIBLE);
+        enterPassword.setVisibility(TextView.INVISIBLE);
+
         buttonScan.setOnClickListener(this);
-        lv = (ListView)findViewById(R.id.list);
+        lv = findViewById(R.id.list);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selected_wifi_ssid = wifinameslist.get(position).get(ITEM_KEY);
+                passwordField.setVisibility(TextView.VISIBLE);
+                enterPassword.setVisibility(TextView.VISIBLE);
+            }
+        });
+        enterPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonScan.setVisibility(View.INVISIBLE);
+                passwordField.setFocusable(false);
+                hideKeyboardFrom(getApplicationContext(), v);
+                selected_wifi_password = passwordField.getText().toString();
+            }
+        });
 
         wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         if (wifi.isWifiEnabled() == false)
@@ -182,7 +98,7 @@ public class WiFiDemo extends Activity implements View.OnClickListener
             Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
             wifi.setWifiEnabled(true);
         }
-        this.adapter = new SimpleAdapter(WiFiDemo.this, arraylist, R.layout.row, new String[] { ITEM_KEY }, new int[] { R.id.list_value });
+        this.adapter = new SimpleAdapter(WiFiDemo.this, wifinameslist, R.layout.row, new String[] { ITEM_KEY }, new int[] { R.id.list_value });
         lv.setAdapter(this.adapter);
 
         registerReceiver(new BroadcastReceiver()
@@ -200,53 +116,47 @@ public class WiFiDemo extends Activity implements View.OnClickListener
                 size = network_results.size();
             }
         }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        //start the wifi scan when app starts
+        wifi.startScan();
     }
 
-    public void onClick(View view)
-    {
-        arraylist.clear();
+    public void onClick(View view) {
+        passwordField.setText("");
+        passwordField.setVisibility(TextView.INVISIBLE);
+        enterPassword.setVisibility(TextView.INVISIBLE);
+        wifinameslist.clear();
         wifi.startScan();
 
         Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
-        try
-        {
+        try {
             size = size - 1;
             ArrayList<String> unique_wifi_ssids = new ArrayList<String>();
-            while (size >= 0)
-            {
+            while (size >= 0) {
                 String current_ssid = network_results.get(size).SSID;
                 HashMap<String, String> item = new HashMap<String, String>();
-                item.put(ITEM_KEY, current_ssid + "  " + network_results.get(size).capabilities);
+                item.put(ITEM_KEY, current_ssid); // + "  " + network_results.get(size).capabilities);
 
-                if(!network_results.get(size).SSID.contains("AirU-") && !unique_wifi_ssids.contains(current_ssid)) {
-                    arraylist.add(item);
+                if (!network_results.get(size).SSID.contains("AirU-") && !unique_wifi_ssids.contains(current_ssid)) {
+                    wifinameslist.add(item);
                 }
-
-                if(!unique_wifi_ssids.contains(current_ssid)){
+                //remove wifi ssids that are not unique
+                if (!unique_wifi_ssids.contains(current_ssid)) {
                     unique_wifi_ssids.add(current_ssid);
                 }
 
                 size--;
                 adapter.notifyDataSetChanged();
             }
-            //display the name of the selected airu device
-            airuDisplay.setText(airu_result.SSID);
+            wifiListHeader.setText("Select a WiFi network:");
+            //displays the name of the selected airu device
+            airuDisplay.setText("Selected AirU device: " + airu_result.SSID);
+        } catch (Exception e) {
         }
-        catch (Exception e)
-        { }
     }
 
-    public static String getCurrentSsid(Context context) {
-        String ssid = null;
-        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (networkInfo.isConnected()) {
-            final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-            final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
-            if (connectionInfo != null && !TextUtils.isEmpty(connectionInfo.getSSID())) {
-                ssid = connectionInfo.getSSID();
-            }
-        }
-        return ssid;
+    public static void hideKeyboardFrom(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
